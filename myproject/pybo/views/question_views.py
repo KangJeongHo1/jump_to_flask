9ef39af321f81022b44ecddcfc736e4af9683f0c
KangJeongHo1 <rkdwjdgh04@naver.com>
2024-03-22 18:55:55 +0900

    240322 오류 발생 수정 예정

--- myproject/pybo/views/question_views.py
+++ myproject/pybo/views/question_views.py
@@ -2,14 +2,10 @@ from flask import Blueprint, render_template
 
 from pybo.models import Question
 
-bp = Blueprint('main', __name__, url_prefix='/')
+bp = Blueprint('main', __name__, url_prefix='/question')
 
-@bp.route('/hello')
-def hello_pybo():
-    return 'Hello, Pybo!'
-
-@bp.route('/')
-def index():
+@bp.route('/list/')
+def _list():
     question_list = Question.query.order_by(Question.create_date.desc())
 
     return render_template('question/question_list.html', question_list=question_list)
